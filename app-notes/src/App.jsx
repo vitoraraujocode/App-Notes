367e868743b587df975036358102f800a74d280a
Vitor <vitor3araujo@gmail.com>
2025-08-13 15:48:07 -0300

    Giving more elements in this project

--- app-notes/src/App.jsx
+++ app-notes/src/App.jsx
@@ -1,7 +1,9 @@
+import NoteForm from "./components/NoteForm";
 const App = () => {
   return (
     <div className="max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg">
       <h2 className="text-2xl font-bold mb-4 text-center">📝 Notes App</h2>
+      <NoteForm />
     </div>
   );
 };
---
+++ app-notes/src/components/NoteForm.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+
+const NoteForm = () => {
+  const [formData, setFormData] = useState({
+    title: "",
+    priority: "Medium",
+    category: "Work",
+    description: "",
+  });
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  return (
+    <form className="mb-6">
+      <div className="mb-4">
+        <label className="block font-semibold">Title:</label>
+        <input
+          type="text"
+          name="title"
+          value={formData.title}
+          className="w-full p-2 border rounded-lg"
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label className="block font-semibold">Priority:</label>
+        <select
+          type="text"
+          name="priority"
+          value={formData.priority}
+          onChange={handleChange}
+          className="w-full p-2 border rounded-lg"
+        >
+          <option value="High">🔴 High</option>
+          <option value="Medium">🟠 Medium</option>
+          <option value="Low">🟢 Low</option>
+        </select>
+      </div>
+      <div className="mb-4">
+        <label className="block font-semibold">Category:</label>
+        <select
+          name="category"
+          value={formData.category}
+          onChange={handleChange}
+          className="w-full p-2 border rounded-lg"
+        >
+          <option value="Work">📂 Work</option>
+          <option value="Personal">🏠 Personal</option>
+          <option value="Ideas">💡 Ideas</option>
+        </select>
+      </div>
+      <div className="mb-4">
+        <label className="block font-semibold">Description:</label>
+        <textarea
+          name="description"
+          value={formData.description}
+          onChange={handleChange}
+          className="w-full p-2 border rounded-lg"
+          required
+        ></textarea>
+      </div>
+      <button className="w-full bg-purple-500 text-white py-2 rounded-lg cursor-pointer hover:bg-purple-600 ">
+        Add Note
+      </button>
+    </form>
+  );
+};
+
+export default NoteForm;
